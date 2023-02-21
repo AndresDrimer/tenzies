751ec67edffd6b4de841229bee97db0384840640
AndresDrimer <andresdrimer@hotmail.com>
2023-02-21 18:32:33 -0300

    timer and score implement, no division min/secs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 import argentina from "/argentina.svg";
 import usa from "/usa.svg";
-import Counter from './components/Counter'
+import Counter from "./components/Counter";
+import dice from "/dice.svg";
 
 function App() {
   const [randomNumsArray, setRandomNumsArray] = useState(populateNumArray());
@@ -23,7 +24,6 @@ function App() {
       setTenzies(true);
     }
   }, [randomNumsArray]);
-
 
   //timer//
   useEffect(() => {
@@ -81,52 +81,75 @@ function App() {
   }
 
   function toggleIdiom() {
-    console.log(spanish);
     setSpanish((prev) => !prev);
   }
 
- const roll = spanish ? "Tirar de nuevo" : "Roll again"
-const btnStyle={
-  backgroundColor: tenzies ? "yellow" : "rgb(136, 189, 216)"
-}
+  const roll = spanish ? "Tirar de nuevo" : "Roll again";
 
-function seconds(n) { return (n < 10 ? '0' : '') + n;}
+  const btnStyle = {
+    backgroundColor: tenzies ? "#222222" : "rgb(136, 189, 216)",
+    color: tenzies ? "azure" : "#222222",
+  };
 
+  function seconds(n) {
+    return (n < 10 ? "0" : "") + n;
+  }
+  const newGame = spanish ? "* Nueva Partida *" : "* New Game *";
+
+  const score = 1000 - (timesPlayed * 20 + counter);
+  
+
+  function reloadANewGame() {
+    setCounter(0);
+    setTenzies(false);
+    setTimesPlayed(0);
+    setRandomNumsArray(populateNumArray());
+  }
 
   return (
     <div className="App">
       <div className="game-wrapper">
         {tenzies && <Confetti />}
         <div className="btns-idiom-selectors">
-          <img 
-          src={ spanish? argentina : usa} 
-          alt="argentinian flag" 
-          onClick={toggleIdiom}
-          className="flag"
+          <img
+            src={spanish ? argentina : usa}
+            alt="argentinian flag"
+            onClick={toggleIdiom}
+            className="flag"
           />
         </div>
-        <h1 className="title">TENZIES</h1>
-        <h3 className="subTitle">{spanish ? "Elegí el número que quieras y marcá todos los que encuentres. Luego tocá el botón ´Tirar de nuevo´ hasta completar todos los casilleros con el mismo número. Apurate, tu tiempo corre!" : "Choose a number and tape on every single one. Update your numbers with the ´Roll Again´ button. Hurry up, time´s running! "}
-          
+
+        <h1 className="title">TENZIES </h1>
+        <img src={dice} width="50px" className="-title-img-dice" />
+        <h3 className="subTitle">
+          {spanish
+            ? "Elegí el número que quieras y marcá todos los que encuentres. Luego tocá el botón ´Tirar de nuevo´ hasta completar todos los casilleros con el mismo número. Apurate, tu tiempo corre!"
+            : "Choose a number and tape on every single one. Update your numbers with the ´Roll Again´ button. Hurry up, time´s running! "}
         </h3>
         <div className="total-numbers-wrapper">{buttonNumbersElements}</div>
-        <button 
-        className="dar-de-nuevo-btn" 
-        onClick={darDeVuelta}
-        style={btnStyle}
-        >
-          {tenzies ? "* Nueva partida *" : roll }
-        </button>
+        {!tenzies ? (
+          <button
+            className="dar-de-nuevo-btn"
+            onClick={darDeVuelta}
+            style={btnStyle}
+          >
+            {tenzies ? newGame : roll}
+          </button>
+        ) : (
+          <button className="btn-play-again" onClick={reloadANewGame}>
+            {spanish ? "* Jugar de nuevo *" : "* New Game *"}
+          </button>
+        )}
         <hr />
-       
-        {timesPlayed > 0 && <h4>{spanish ? "Cantidad de tiradas" : "Number of rows"}: {timesPlayed}</h4>}
 
-        <Counter 
-        spanish={spanish}
-        seconds={seconds}
-        counter={counter}
+        <Counter
+          spanish={spanish}
+          seconds={seconds}
+          counter={counter}
+          timesPlayed={timesPlayed}
+          score={score}
         />
-       
+        
       </div>
     </div>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,22 @@
 export default function Counter(props) {
   return (
-    <div className="--counter-wrapper">
-      <h5>
-        {props.spanish ? "Tiempo: " : "Timer: "}
-        {props.seconds(props.counter)}
-      </h5>
+    <div className="--counter-wrapper-total">
+      <div className="--counter-wrapper-first-line"> 
+        {props.timesPlayed > 0 && (
+          <h4>{props.spanish ? "Cantidad de tiradas" : "Number of rows"}:{" "}
+            {props.timesPlayed}
+          </h4>)}
+        <div className="--counter-wrapper">
+          <h4>
+            {props.spanish ? "Tiempo: " : "Timer: "}
+            {props.seconds(props.counter)}
+          </h4>
+        </div>
+      </div>
+        <div className="--counter-total-points">
+          <h3>{props.spanish ? "PUNTAJE " : "SCORE "} : {props.score} </h3>
+        </div>
+      
     </div>
   );
 }
